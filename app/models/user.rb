72428a863647d741a293f534afb88c092b58bc48
stochastic-thread <hello@arthurcolle.com>
2019-05-04 15:21:43 -0400

    Added seeds with correct model schema and relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :loan_inquiries
+  has_many :loan_inquiries, foreign_key: "borrower_id", class_name: "LoanInquiry"
 end
