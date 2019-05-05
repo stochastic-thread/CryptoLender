de0c4a394248d2aa0f90d00da104b89937db1da5
Andrew <akinhwan@gmail.com>
2019-05-04 22:04:11 -0400

    route to profiles datatable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def profiles
+  end  
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   end
   get '/toggle',          to: "users#toggle"
   get '/showViableFunds', to: "users#showViableFunds"
+  get '/investor_profiles', to: "pages#profiles"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
 end
