2e873278eba8acd7ea78a4c2100c320674220528
stochastic-thread <hello@arthurcolle.com>
2019-05-05 03:23:00 -0400

    Transaction handling in case of earthquake

--- app/controllers/payment_gateway_controller.rb
+++ app/controllers/payment_gateway_controller.rb
@@ -28,17 +28,17 @@ class PaymentGatewayController < ApplicationController
     ap "Routing number (test): #{routingNumber}"
 
 
-    request = CreateTransactionRequest.new
-    request.transactionRequest = TransactionRequestType.new()
-    request.transactionRequest.amount = ((SecureRandom.random_number + 1 ) * 15 ).round(2)
-    request.transactionRequest.payment = PaymentType.new
+    # request = CreateTransactionRequest.new
+    # request.transactionRequest = TransactionRequestType.new()
+    # request.transactionRequest.amount = ((SecureRandom.random_number + 1 ) * 15 ).round(2)
+    # request.transactionRequest.payment = PaymentType.new
     #Generate random bank account number
     randomAccountNumber= Random.rand(100000000..9999999999).to_s
-    request.transactionRequest.payment.bankAccount = BankAccountType.new('checking',"#{routingNumber}","#{accountNumber}", 'John Doe','WEB','Wells Fargo Bank NA','101')
-    request.transactionRequest.transactionType = TransactionTypeEnum::AuthCaptureTransaction
-    request.transactionRequest.order = OrderType.new("invoiceNumber#{(SecureRandom.random_number*1000000).round(0)}","Order Description")
+    request_test.transactionRequest.payment.bankAccount = BankAccountType.new('checking',"#{routingNumber}","#{accountNumber}", 'John Doe','WEB','Wells Fargo Bank NA','101')
+    request_test.transactionRequest.transactionType = TransactionTypeEnum::AuthCaptureTransaction
+    request_test.transactionRequest.order = OrderType.new("invoiceNumber#{(SecureRandom.random_number*1000000).round(0)}","Order Description")
 
-    response = transaction.create_transaction(request)
+    @response = transaction.create_transaction(request_test)
 
 
 
@@ -57,31 +57,31 @@ class PaymentGatewayController < ApplicationController
     #
     # request.transactionRequest.order = OrderType.new("invoiceNumber#{(SecureRandom.random_number*1000000).round(0)}","Order Description")
     # response = transaction.create_transaction(request)
-    if response != nil
-      if response.messages.resultCode == MessageTypeEnum::Ok
-        if response.transactionResponse != nil && (response.transactionResponse.messages != nil)
+    if @response != nil
+      if @response.messages.resultCode == MessageTypeEnum::Ok
+        if @response.transactionResponse != nil && (@response.transactionResponse.messages != nil)
           puts "Successfully debited bank account."
-          puts "  Transaction ID: #{response.transactionResponse.transId}"
-          puts "  Transaction response code: #{response.transactionResponse.responseCode}"
-          puts "  Code: #{response.transactionResponse.messages.messages[0].code}"
-          puts "  Description: #{response.transactionResponse.messages.messages[0].description}"
+          puts "  Transaction ID: #{@response.transactionResponse.transId}"
+          puts "  Transaction response code: #{@response.transactionResponse.responseCode}"
+          puts "  Code: #{@response.transactionResponse.messages.messages[0].code}"
+          puts "  Description: #{@response.transactionResponse.messages.messages[0].description}"
         else
           puts "Transaction Failed"
-          puts "Transaction response code: #{response.transactionResponse.responseCode}"
-          if response.transactionResponse.errors != nil
-            puts "  Error Code: #{response.transactionResponse.errors.errors[0].errorCode}"
-            puts "  Error Message: #{response.transactionResponse.errors.errors[0].errorText}"
+          puts "Transaction response code: #{@response.transactionResponse.responseCode}"
+          if @response.transactionResponse.errors != nil
+            puts "  Error Code: #{@response.transactionResponse.errors.errors[0].errorCode}"
+            puts "  Error Message: #{@response.transactionResponse.errors.errors[0].errorText}"
           end
           puts "Failed to debit bank account."
         end
       else
         puts "Transaction Failed"
-        if response.transactionResponse != nil && response.transactionResponse.errors != nil
-          puts "  Error Code: #{response.transactionResponse.errors.errors[0].errorCode}"
-          puts "  Error Message: #{response.transactionResponse.errors.errors[0].errorText}"
+        if @response.transactionResponse != nil && @response.transactionResponse.errors != nil
+          puts "  Error Code: #{@response.transactionResponse.errors.errors[0].errorCode}"
+          puts "  Error Message: #{@response.transactionResponse.errors.errors[0].errorText}"
         else
-          puts "  Error Code: #{response.messages.messages[0].code}"
-          puts "  Error Message: #{response.messages.messages[0].text}"
+          puts "  Error Code: #{@response.messages.messages[0].code}"
+          puts "  Error Message: #{@response.messages.messages[0].text}"
         end
         puts "Failed to debit bank account."
       end
@@ -89,8 +89,7 @@ class PaymentGatewayController < ApplicationController
       puts "Response is null"
       raise "Failed to debit bank account."
     end
-
-    return response
+    @response
   end
 
   def mock_account_number
